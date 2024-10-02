9fda752196cb6bba8d1aa3a785d03c77050a796f
Maria Clara Garatini <maria.garatini.senai404@gmail.com>
2024-10-02 13:55:28 -0300

    Delete PHP directory

--- PHP/exercicio8.php
+++
@@ -1,7 +0,0 @@
-<?php
-$soma = 0;
-for ($i = 1; $i <= 100; $i++){
-    $soma += $i;   
-}
-echo "A soma de todos os número de 1 até 100 é: $soma";
-?>
--- PHP/exercicio9.php
+++
@@ -1,9 +0,0 @@
-<?php
-$raiz = 0;
-$raiz2 = 0;
-for($i = 1; $i <= 50; $i++){
-    $raiz = sqrt($i);
-    $raiz2 = round($raiz, 2);
-    echo "As raizes de $i: $raiz2 </br>";
-}
-?>
