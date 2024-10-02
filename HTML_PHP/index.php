0564d786a307f1205a6c5809fba2c1e045bac48b
Maria Clara Garatini <maria.garatini.senai404@gmail.com>
2024-10-02 14:05:42 -0300

    Add files via upload

---
+++ HTML_PHP/index.php
@@ -0,0 +1,20 @@
+<?php
+    $nome = 'Valter';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML - PHP</title>
+</head>
+<body>
+    <h1> Bem vindo 
+        <?php 
+            echo $nome;
+            echo ' <h1> Olá meu SENAI';
+        ?>
+    </h1>
+</body>
+</html>
